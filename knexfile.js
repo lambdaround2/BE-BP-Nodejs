b2095028a2562f44374825e4cf10012973ad5172
amlane <twelvexstring@gmail.com>
2019-08-28 18:09:27 -0500

    added production settings for pg

--- knexfile.js
+++ knexfile.js
@@ -18,5 +18,16 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     }
+  },
+  production: {
+    client: "pg",
+    useNullAsDefault: true,
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
   }
 };
